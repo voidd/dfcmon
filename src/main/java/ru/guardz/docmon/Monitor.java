8775bebda90bb141f0c8079b36eef1d3043fe8ac
Ivan <void@guardz.ru>
2013-02-28 17:00:25 +0400

    fix

--- src/main/java/ru/guardz/docmon/Monitor.java
+++ src/main/java/ru/guardz/docmon/Monitor.java
@@ -1,25 +1,17 @@
+package ru.guardz.docmon;
+
 import com.documentum.com.DfClientX;
 import com.documentum.com.IDfClientX;
-import com.documentum.fc.client.IDfClient;
-import com.documentum.fc.client.IDfSession;
-import com.documentum.fc.client.IDfSessionManager;
-import com.documentum.fc.common.DfCriticalException;
+import com.documentum.fc.client.*;
 import com.documentum.fc.common.DfException;
 import com.documentum.fc.common.DfLogger;
 import com.documentum.fc.common.IDfLoginInfo;
 
-public class Main {
+public class Monitor {
 
     public static void main(String[] args) throws DfException {
-        if (args.length < 4) {
-            String message = "Wrong parameter's number. Must be at least 4 parameters: "
-                    + "<user_name> <password> <repository> <fullpath_filename>";
-//            DfLogger.error(UpdateDpkFromFile.class, message, null, null);
-            throw new DfCriticalException(message);
-        }
-
         String docbaseName = args[2];
-        IDfSessionManager dfSessionManager = connect2Dctm(args);
+        IDfSessionManager dfSessionManager = initialConnect(args);
 
         IDfSession dfSession = null;
         try {
@@ -30,21 +22,40 @@ public class Main {
 /*            CustomProxyFactoriesLocator.registerAll();
 
             updateDpk(args, dfSession);*/
+            System.out.println("JMS Config".concat(getJMSConfig(dfSession)));
 
-            System.out.println("UpdateDpkFromFile Successfully complete");
+
+            System.out.println("JMS Config Successfully complete");
         } catch (Throwable t) {
             DfLogger.fatal(dfSessionManager, t.getMessage(), null, t);
-        } finally {
-            if (dfSessionManager != null) {
-                dfSessionManager.release(dfSession);
-            }
         }
-
-        System.exit(0);
-	// write your code here
+        // write your code here
     }
 
-    private static IDfSessionManager connect2Dctm(String[] args) throws DfException {
+    private static String getJMSConfig(IDfSession dfSession) throws DfException {
+        final String s = "SELECT * FROM DM_JMS_CONFIG";
+        IDfQuery query = new DfQuery();
+        query.setDQL(s);
+        String jmsName = null;
+        int count = 0;
+        IDfCollection collection = query.execute(dfSession,IDfQuery.DF_READ_QUERY);
+        try {
+            while (collection.next()) {
+                count++;
+                jmsName = collection.getString("object_name");
+            }
+        } catch (DfException e) {
+            e.printStackTrace();
+        } finally {
+            // ALWAYS! clean up your collections
+            if (collection != null) {
+                collection.close();
+            }
+        }
+        return jmsName;
+    }
+
+    private static IDfSessionManager initialConnect(String[] args) throws DfException {
         IDfClientX clientx = new DfClientX();
         IDfClient client = clientx.getLocalClient();
         IDfLoginInfo iLogin = clientx.getLoginInfo();
