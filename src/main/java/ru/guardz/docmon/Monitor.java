463b5e1bfa0e5520304e29c1376883063c08a592
Ivan <void@guardz.ru>
2013-03-29 15:32:04 +0400

    fix

--- src/main/java/ru/guardz/docmon/Monitor.java
+++ src/main/java/ru/guardz/docmon/Monitor.java
@@ -7,7 +7,6 @@ import com.documentum.fc.common.DfException;
 import com.documentum.fc.common.DfLogger;
 import com.documentum.fc.common.IDfId;
 import com.documentum.fc.common.IDfLoginInfo;
-import ru.guardz.docmon.IndexAgentInfo;
 
 import java.io.File;
 import java.io.IOException;
@@ -39,6 +38,8 @@ public class Monitor {
         return count;
     }
 
+    ;
+
     private static List getIndexName(IDfSession dfSession) throws DfException {
         List result = new ArrayList<String>();
         IDfCollection col;
@@ -50,22 +51,22 @@ public class Monitor {
         );
         col = q.execute(dfSession, 0);
         while (col.next()) {
-            result.add(new IndexAgentInfo(col.getString("index_name").trim(),col.getString("instance_name").trim()));
+            result.add(new IndexAgentInfo(col.getString("index_name").trim(), col.getString("instance_name").trim()));
         }
         col.close();
         return result;
     }
 
     private static Integer getDeadWorkflows(IDfSession dfSession) throws DfException {
-        final String s = "SELECT r_object_id FROM dm_workflow w WHERE any w.r_act_state in (3,4)";
+        final String s = "SELECT count(*) as cnt FROM dm_workflow w WHERE any w.r_act_state in (3,4)";
         IDfQuery query = new DfQuery();
         query.setDQL(s);
         int count = 0;
         IDfCollection collection = query.execute(dfSession, IDfQuery.DF_READ_QUERY);
         try {
-            while (collection.next()) {
-                count++;
-            }
+                if (collection.next()) {
+                    count = collection.getInt("cnt");
+                }
         } catch (DfException e) {
             e.printStackTrace();
         } finally {
@@ -77,14 +78,14 @@ public class Monitor {
     }
 
     private static Integer getBadWorkitems(IDfSession dfSession) throws DfException {
-        final String s = "select * from dmi_workitem w, dm_workflow wf where  w.r_workflow_id = wf.r_object_id and a_wq_name not in (select r_object_id from dm_server_config)";
+        final String s = "select count(*) as cnt from dmi_workitem w, dm_workflow wf where  w.r_workflow_id = wf.r_object_id and a_wq_name not in (select r_object_id from dm_server_config)";
         IDfQuery query = new DfQuery();
         query.setDQL(s);
         int count = 0;
         IDfCollection collection = query.execute(dfSession, IDfQuery.DF_READ_QUERY);
         try {
-            while (collection.next()) {
-                count++;
+            if (collection.next()) {
+                count = collection.getInt("cnt");
             }
         } catch (DfException e) {
             e.printStackTrace();
@@ -97,7 +98,27 @@ public class Monitor {
     }
 
     private static Integer getFTQueueSize(IDfSession dfSession) throws DfException {
-        final String s = "select count(*) from dmi_queue_item where name = 'dm_fulltext_index_user' and task_state not in ('failed','warning')";
+        final String s = "select count(*) as cnt from dmi_queue_item where name = 'dm_fulltext_index_user' and task_state not in ('failed','warning')";
+        IDfQuery query = new DfQuery();
+        query.setDQL(s);
+        int count = 0;
+        IDfCollection collection = query.execute(dfSession, IDfQuery.DF_READ_QUERY);
+        try {
+            if (collection.next()) {
+                count = collection.getInt("cnt");
+            }
+        } catch (DfException e) {
+            e.printStackTrace();
+        } finally {
+            if (collection != null) {
+                collection.close();
+            }
+        }
+        return count;
+    }
+
+    private static Boolean checkFTSearch(IDfSession dfSession) throws DfException {
+        final String s = "select r_object_id from dm_sysobject SEARCH DOCUMENT CONTAINS 'test' enable(return_top 1)";
         IDfQuery query = new DfQuery();
         query.setDQL(s);
         int count = 0;
@@ -113,12 +134,12 @@ public class Monitor {
                 collection.close();
             }
         }
-        return count;
+        return count >= 1;
     }
 
     private static Boolean fetchContent(IDfSession dfSession) throws DfException, IOException {
         isConnected();
-        final String s = "select * from dm_document where folder('/System/Sysadmin/Reports') enable (RETURN_TOP 1)";
+        final String s = "select r_object_id from dm_document where folder('/System/Sysadmin/Reports') enable (RETURN_TOP 1)";
         IDfQuery query = new DfQuery();
         query.setDQL(s);
         Boolean ret;
@@ -200,6 +221,7 @@ public class Monitor {
             System.out.println("Total workitems not associated with servers: ".concat(getBadWorkitems(dfSession).toString()));
             System.out.println("IndexAgent status: ".concat(statusOfIA(dfSession)));
             System.out.println("Total number of queued items: ".concat(getFTQueueSize(dfSession).toString()));
+            System.out.println("Fulltext Search status: ".concat((checkFTSearch(dfSession).toString())));
 
             if (fetchContent(dfSession)) System.out.println("Can fetch content!");
 
@@ -216,39 +238,58 @@ public class Monitor {
         List list = getIndexName(dfSession);
         IndexAgentInfo agentInfo;
         for (Object aList : list) {
-             agentInfo = (IndexAgentInfo) aList;
+            agentInfo = (IndexAgentInfo) aList;
 
-        String instanceName = agentInfo != null ? agentInfo.get_instance_name() : null;
-        String indexName = agentInfo != null ? agentInfo.get_index_name() : null;
-        String query = "NULL,FTINDEX_AGENT_ADMIN,NAME,S," +
-                indexName + ",AGENT_INSTANCE_NAME,S," + instanceName + ",ACTION,S,status";
-        DfClientX clientX = new DfClientX();
-        IDfQuery q = clientX.getQuery();
-        q.setDQL(query);
-        IDfCollection collection = q.execute(dfSession, IDfQuery.DF_APPLY);
-        try {
-            dfSession.getMessage(1);
-            collection.next();
-            int count = collection.getValueCount("name");
-            for (int ix = 0; ix < count; ix++) {
-                String indexAgentName = collection.getRepeatingString("name", ix);
-                String status = collection.getRepeatingString("status", ix);
-                if (Integer.parseInt(status) == 200) {
-                    ret = indexAgentName.concat("is in not responsible state");
-                } else if(Integer.parseInt(status) == 100) {
-                    ret = indexAgentName.concat("is shutdown");
-                } else if(Integer.parseInt(status) == 0) {
-                    ret = indexAgentName.concat(" is running");
+            String instanceName = agentInfo != null ? agentInfo.get_instance_name() : null;
+            String indexName = agentInfo != null ? agentInfo.get_index_name() : null;
+            String query = "NULL,FTINDEX_AGENT_ADMIN,NAME,S," +
+                    indexName + ",AGENT_INSTANCE_NAME,S," + instanceName + ",ACTION,S,status";
+            DfClientX clientX = new DfClientX();
+            IDfQuery q = clientX.getQuery();
+            q.setDQL(query);
+            IDfCollection collection = q.execute(dfSession, IDfQuery.DF_APPLY);
+            try {
+                dfSession.getMessage(1);
+                collection.next();
+                int count = collection.getValueCount("name");
+                for (int ix = 0; ix < count; ix++) {
+                    String indexAgentName = collection.getRepeatingString("name", ix);
+                    String status = collection.getRepeatingString("status", ix);
+                    if (Integer.parseInt(status) == 200) {
+                        ret = indexAgentName.concat("is in not responsible state");
+                    } else if (Integer.parseInt(status) == 100) {
+                        ret = indexAgentName.concat("is shutdown");
+                    } else if (Integer.parseInt(status) == 0) {
+                        ret = indexAgentName.concat(" is running");
+                    }
+                }
+            } catch (DfException exception) {
+                exception.printStackTrace();
+            } finally {
+                if (collection != null) {
+                    collection.close();
                 }
             }
-        } catch (DfException exception) {
-            exception.printStackTrace();
-        } finally {
-            if (collection != null) {
-                collection.close();
-            }
         }
+        return ret;
+    }
+
+    public static class IndexAgentInfo {
+        private String m_index_name;
+        private String m_instance_name;
+
+        public IndexAgentInfo(String index_name, String instance_name) {
+            this.m_index_name = index_name;
+            this.m_instance_name = instance_name;
         }
-       return ret;
+
+        public String get_index_name() {
+            return m_index_name;
+        }
+
+        public String get_instance_name() {
+            return m_instance_name;
+        }
+
     }
 }
