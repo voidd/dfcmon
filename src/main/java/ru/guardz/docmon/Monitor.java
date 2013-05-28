1c1b2e1eb93ce15e7c7981f083818113dc4c8a51
Ivan <void@guardz.ru>
2013-05-28 18:09:47 +0400

    imports

--- src/main/java/ru/guardz/docmon/Monitor.java
+++ src/main/java/ru/guardz/docmon/Monitor.java
@@ -7,7 +7,12 @@ import com.documentum.fc.common.DfException;
 import com.documentum.fc.common.DfLogger;
 import com.documentum.fc.common.IDfId;
 import com.documentum.fc.common.IDfLoginInfo;
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
 
 import java.io.File;
 import java.io.IOException;
